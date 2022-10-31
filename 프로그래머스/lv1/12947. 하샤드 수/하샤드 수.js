affe3a576ac775660dbbe695de95f2145fa806da
예지 <99022588+jeong9204@users.noreply.github.com>
2022-10-31 14:55:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,14 +1,7 @@
 function solution(x) {
-    var answer = true;
     
     let arr = (x+'').split('').map(Number);
     let sum = arr.reduce((a,b) => a+b);
     
-    if(x % sum !== 0) {
-        answer = false;
-    } else {
-        answer = true;
-    }
-    
-    return answer;
+    return x % sum == 0;
 }

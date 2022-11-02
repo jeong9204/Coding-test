96bc76440f19f158e229fffe2b941c9cb8c9c547
예지 <99022588+jeong9204@users.noreply.github.com>
2022-11-02 10:53:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,6 +1,7 @@
 function solution(s){
-    let p = [...s].filter(el => el === 'p' || el === 'P');
-    let y = [...s].filter(el => el === 'y' || el === 'Y');
+    let arr = [...s]
+    let p = arr.filter(el => el === 'p' || el === 'P');
+    let y = arr.filter(el => el === 'y' || el === 'Y');
 
     return p.length === y.length ? true : false;
 }

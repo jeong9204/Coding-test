f3902733c96c17ecb5ea7273525e23cd808d97b6
예지 <99022588+jeong9204@users.noreply.github.com>
2022-11-03 13:02:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,8 +1,6 @@
 function solution(num) {
     let count = 0;
     
-    if(num === 1) return 0
-    
     while (num > 1) {
         if(num % 2 === 0) {
            num = num/2;
@@ -10,7 +8,6 @@ function solution(num) {
             num = (num*3)+1;
         }
         count++;
-        if(num === 1) {break;} 
         if(count > 500) {
             count = -1;
             break;

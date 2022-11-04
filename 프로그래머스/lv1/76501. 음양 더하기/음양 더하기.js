223ff71a81342e9f372f5c194d8ede03f0efca2c
예지 <99022588+jeong9204@users.noreply.github.com>
2022-11-04 17:15:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,16 @@
 function solution(absolutes, signs) {
-    let arr = [];
-    for(let i =0; i<absolutes.length; i++) {
-        if(signs[i] === true) {
-            arr.push(absolutes[i])
-        } else {
-             arr.push(absolutes[i] * -1)
-        }
-    }
-    return arr.reduce((a,b) => a+b)
+    // let arr = [];
+    // for(let i =0; i<absolutes.length; i++) {
+    //     if(signs[i] === true) {
+    //         arr.push(absolutes[i])
+    //     } else {
+    //          arr.push(absolutes[i] * -1)
+    //     }
+    // }
+    // return arr.reduce((a,b) => a+b)
+    
+   return absolutes.reduce((a,b,i) => {
+       if(signs[i] === false) {b= -b}
+        return a + b
+    },0)
 }

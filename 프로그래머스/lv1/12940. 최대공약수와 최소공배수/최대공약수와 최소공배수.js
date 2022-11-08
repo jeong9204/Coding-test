c82b9016b79428ec7e33e1a96ba1b749888c6a92
예지 <99022588+jeong9204@users.noreply.github.com>
2022-11-08 14:37:44 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.21 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,13 +1,22 @@
 function solution(n, m) {
-    var gcd = 0;
+    let gcd = 0;
+    let arr = [];
     let minNum = n>m ? m : n;
     let maxNum = n>m ? n : m;
     
     for(let i=minNum; i>=1; i--) {
         if(n % i === 0 && m % i === 0) {
-            gcd = i
+            //gcd = i
+            arr.push(i)
             break;
         }
     }
-    return [gcd, ((n*m)/gcd)];
+    for(let j=maxNum; maxNum<=(n*m); j++) {
+        if(j%n === 0 && j%m === 0) {
+            arr.push(j)
+            break;
+        }
+    }
+    return arr;
+    //return [gcd, ((n*m)/gcd)];
 }

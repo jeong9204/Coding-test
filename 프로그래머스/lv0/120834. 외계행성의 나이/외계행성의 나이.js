91079f3cbc4e2006a9eb4e4133e708dac58304af
예지 <99022588+jeong9204@users.noreply.github.com>
2022-12-09 14:42:33 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,6 +1,6 @@
 function solution(age) {
     var answer = [];
-    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let alphabet = ['a','b','c','d','e','f','g','h','i','j'];
     let arr = age.toString().split('');
     for(let i=0; i<arr.length; i++) {
         answer.push(alphabet[arr[i]])

82f4d17387c5797597378ba8fa1e49ebc19b60e5
예지 <99022588+jeong9204@users.noreply.github.com>
2023-07-31 21:36:36 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,11 @@
+function solution(a, b, c) {
+    var answer = 0;
+    if (a!==b && b!==c && a!==c) {
+           answer += a+b+c
+       } else if(a===b && b===c && a===c) {
+       answer += (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3))
+       } else if(a!==b===c || a===b!==c) {
+          answer += (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))
+       } 
+    return answer;
+}

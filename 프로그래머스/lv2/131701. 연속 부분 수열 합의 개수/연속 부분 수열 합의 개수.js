209fa7c62846bc6e4d13e78bd030b14d90dc1132
예지 <99022588+jeong9204@users.noreply.github.com>
2023-09-20 21:46:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1084.43 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,23 @@
+function solution(elements) {
+    const set = new Set();
+    for(let i=0; i<elements.length; i++) {
+        const els = elements.concat(elements.slice(0,i));
+        for(let j=0; j<elements.length; j++) {
+            set.add(els.slice(j,j+i).reduce((a,b) => a+b,0))
+        }
+    }
+    return set.size;
+}
+
+// function solution(elements) {
+//   const set = new Set();
+//   for (let i = 1; i <= elements.length; i++) {
+//     // 원순열 만들기 i개의 원소를 뒤에 추가해준다
+//     const els = elements.concat(elements.slice(0, i));
+//     // set 갱신
+//     for (let j = 0; j < elements.length; j++) {
+//       set.add(els.slice(j, j + i).reduce((a, c) => a + c, 0));
+//     }
+//   }
+//   return set.size;
+// }

20acd2c6d2730f1eefd045a2ea73162c5cb6e946
예지 <99022588+jeong9204@users.noreply.github.com>
2023-09-30 23:29:31 +0900

    [level 2] Title: 튜플, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,25 @@
+function solution(s) {
+    var answer = [];
+    const arr = JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'));
+    arr.sort((a,b) => a.length - b.length);
+    arr.forEach((col) => {
+        col.forEach((row) => {
+            if(!answer.includes(row)) {
+                answer.push(row)
+            }
+        })
+    })
+    return answer;
+}
+
+// function solution(s) {
+//   const answer = [];
+//   const arr = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"));
+//   arr.sort((a, b) => a.length - b.length);
+//   arr.forEach((col) => {
+//     col.forEach((row) => {
+//       if (!answer.includes(row)) answer.push(row);
+//     });
+//   });
+//   return answer;
+// }

febf1a3681061700771a65f3c206b7ee62d9567a
예지 <99022588+jeong9204@users.noreply.github.com>
2024-06-22 22:33:30 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+for (let i = 1; i <= input[0]; i++) {
+  let numbers = input[i].split(' ');
+
+  let num1 = Number(numbers[0]);
+  let num2 = Number(numbers[1]);
+
+  console.log(`Case #${i}: ${num1 + num2}`);
+}

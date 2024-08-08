e3e06f76e4a11099f184f73d43f15659339998e9
예지 <99022588+jeong9204@users.noreply.github.com>
2024-08-08 21:44:48 +0900

    [Bronze V] Title: 연세대학교, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+if(input === '0') {
+    console.log('YONSEI')
+} else if(input === '1') {
+    console.log('Leading the Way to the Future')
+}
